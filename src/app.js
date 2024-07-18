64c2c581c16a5e6e3d35ee20b17e6010c3e2f028
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-07-18 02:48:36 +0000

    JochiKum-DomainGenerator

--- src/app.js
+++ src/app.js
@@ -6,10 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  let pronoun = ["the", "our"];
+  let pronombre = ["the", "our"];
   let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
+  let nombre = ["jogger", "racoon"];
+  let extensiones = [".com", ".net"];
+  let myList = document.querySelector("#lista");
+  let listaNombresDominios = "";
 
-  console.log(pronoun);
-};
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let a = 0; a < adj.length; a++ ) {
+      for (let n = 0; n < nombre.length; n++ ) {
+        for (let j = 0; j < extensiones.length; j++ ) {
+          listaNombresDominios += '<li>' + pronombre[i] + adj[a] + nombre[n] + extensiones[j] + '</li>';
+
+        }
+      }
+    }
+  }
+  myList.innerHTML = listaNombresDominios;
+}
